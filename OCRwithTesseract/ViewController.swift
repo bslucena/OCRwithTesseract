38f6546a4d8350f2dd048d0c1f32b431cf587b75
Pernas <bslucena@gmail.com>
2018-04-27 15:20:14 +0200

    Uploaded the model for incosolata font

--- OCRwithTesseract/ViewController.swift
+++ OCRwithTesseract/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
 class ViewController: UIViewController {
+    
+    lazy var ocrRequest: VNCoreMLRequest = {
+        do {
+            //THIS MODEL IS TRAINED BY ME FOR FONT "Inconsolata" (Numbers 0...9 and UpperCase Characters A..Z)
+            let model = try VNCoreMLModel(for:OCR().model)
+            return VNCoreMLRequest(model: model, completionHandler: self.handleClassification)
+        } catch {
+            fatalError("cannot load model")
+        }
+    }()
 
+    func handleClassification(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNClassificationObservation]
+            else { fatalError("Unexpected result!") }
+        guard let best = observations.first
+            else { fatalError("Can't get best resutl!") }
+        
+        // on main queue
+        DispatchQueue.main.async {
+            // best.identifier is our prediction with highest confidence
+            print ("Recognized \(best.identifier)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // lets load an image from resource
+        let ourFirstImage: UIImage = UIImage(named: "7")!
+        
+        // we need an ciimage - no need to scale
+        let ourInput: CIImage = CIImage(image: ourFirstImage)!
+        
+        // prepare the handler
+        let handler = VNImageRequestHandler(ciImage: ourInput, options: [:])
+        
+        // some options
+        self.ocrRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.scaleFill
+        
+        // feed to the queue
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([self.ocrRequest])
+            } catch {
+                print("Error")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
